329cd7dd06097577b7a7845a856f7552fecc7ab7
Tan-Tien-Le <duytan3105@gmail.com>
2024-12-06 15:03:56 +0700

    feat: delivery api

---
+++ Backend/src/main/java/org/example/backend/controllers/DeliveryInfoController.java
@@ -0,0 +1,32 @@
+package org.example.backend.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.backend.dtos.responses.AIMSResponse;
+import org.example.backend.entities.delivery.DeliveryInfo;
+import org.example.backend.services.DeliveryInfoService;
+import org.example.backend.utils.StringUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${api.prefix}/delivery-info")
+@RequiredArgsConstructor
+public class DeliveryInfoController {
+    private final DeliveryInfoService deliveryInfoService;
+
+    @PostMapping("/create")
+    public ResponseEntity<AIMSResponse<Object>> createDeliveryInfo(DeliveryInfo request) {
+        StringUtils.trimAllStringFields(request);
+        return deliveryInfoService.createDeliveryInfo(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AIMSResponse<Object>> getDeliveryInfo(@PathVariable String id) {
+        return deliveryInfoService.getDeliveryInfo(id);
+    }
+
+    @GetMapping("/shipping-fee")
+    public ResponseEntity<AIMSResponse<Object>> calculateShippingFee(@RequestParam String province, @RequestParam boolean isRushDelivery) {
+        return deliveryInfoService.calculateShippingFee(province, isRushDelivery);
+    }
+}
